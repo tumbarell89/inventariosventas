3791ab05afd61ada6a510e7d8d294f42f6b4d08e
tumbarell89 <95498784+tumbarell89@users.noreply.github.com>
2024-12-10 14:44:18 -0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,39 +1,8 @@
 import { Sequelize } from 'sequelize';
+import pg from 'pg'; // Importa explícitamente el módulo 'pg'
 import dotenv from 'dotenv';
 
 dotenv.config();
-/*
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectModule: require('pg'),
-  logging: false, // set to console.log to see the raw SQL queries
-  ssl: process.env.NODE_ENV === 'production',
-  pool: {
-    max: 5, // Número máximo de conexiones
-    min: 0, // Número mínimo de conexiones
-    acquire: 30000, // Tiempo máximo en milisegundos para adquirir conexión
-    idle: 10000, // Tiempo máximo que una conexión puede estar inactiva
-  },
-});
-
-user= password=[YOUR-PASSWORD] host=aws-0-us-west-1.pooler.supabase.com port=6543 dbname=postgres
-const database = new sequelize({
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    dialect: 'postgres',
-    dialectModule: require('pg'),
-    logging: false,
-    dialectOptions: {
-        timezone: process.env.db_timezone
-    },
-    define: {
-        freezeTableName: true,
-        timestamps: false
-    }
-});*/
 
 const sequelize = new Sequelize({
     host: 'aws-0-us-west-1.pooler.supabase.com',
@@ -42,7 +11,7 @@ const sequelize = new Sequelize({
     password: 'ControlOfertas',
     database: 'controlinventario',
     dialect: 'postgres',
-    dialectModule: require('pg'),
+    dialectModule: pg, // Usa el módulo importado
     logging: false,
     define: {
         freezeTableName: true,
@@ -50,12 +19,11 @@ const sequelize = new Sequelize({
     },
     ssl: process.env.NODE_ENV === 'production',
     pool: {
-      max: 5, // Número máximo de conexiones
-      min: 0, // Número mínimo de conexiones
-      acquire: 30000, // Tiempo máximo en milisegundos para adquirir conexión
-      idle: 10000, // Tiempo máximo que una conexión puede estar inactiva
+        max: 5, // Número máximo de conexiones
+        min: 0, // Número mínimo de conexiones
+        acquire: 30000, // Tiempo máximo en milisegundos para adquirir conexión
+        idle: 10000, // Tiempo máximo que una conexión puede estar inactiva
     },
 });
 
 export default sequelize;
-
